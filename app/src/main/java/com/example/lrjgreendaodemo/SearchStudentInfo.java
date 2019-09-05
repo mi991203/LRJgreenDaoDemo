10a601ed2e7af1c7bc7ceaa059eb990c808fc9a4
mi991203 <1101315853@qq.com>
2019-09-05 10:13:52 +0800

    9.4.1

--- app/src/main/java/com/example/lrjgreendaodemo/SearchStudentInfo.java
+++ app/src/main/java/com/example/lrjgreendaodemo/SearchStudentInfo.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,12 +21,13 @@ public class SearchStudentInfo extends AppCompatActivity {
 
         etSearchStudentInfoSno = findViewById(R.id.et_search_student_info_sno);
         btSearchStudentInfoBySno = findViewById(R.id.bt_search_student_info_bySno);
-        str_et_search_stu_by_sno = etSearchStudentInfoSno.getText().toString();
 
         btSearchStudentInfoBySno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SearchStudentInfo.this, StudentList.class);
+                str_et_search_stu_by_sno = etSearchStudentInfoSno.getText().toString();
+                Log.e("SH", str_et_search_stu_by_sno);
                 intent.putExtra("search_stu_by_sno", str_et_search_stu_by_sno);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/lrjgreendaodemo/StudentList.java
+++ app/src/main/java/com/example/lrjgreendaodemo/StudentList.java
@@ -4,16 +4,20 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.example.lrjgreendaodemo.gen.DaoMaster;
 import com.example.lrjgreendaodemo.gen.DaoSession;
 import com.example.lrjgreendaodemo.gen.StudentDao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentList extends AppCompatActivity {
     private List<Student> students_list;
+    private List<Student> students_list_by_sno;
+    String str;
     private DaoMaster master;
     private DaoSession session;
     private StudentDao studentDao;
@@ -24,6 +28,8 @@ public class StudentList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_list);
         openDb();
+        str = getIntent().getStringExtra("search_stu_by_sno");
+        students_list_by_sno = new ArrayList<>();
     }
 
     private void openDb() {
@@ -41,8 +47,21 @@ public class StudentList extends AppCompatActivity {
 
     private void load() {
         students_list = studentDao.queryBuilder().list();
-        StudentAdapter adapter = new StudentAdapter(StudentList.this, R.layout.student_list_item, students_list);
-        ListView listView = findViewById(R.id.student_info_list);
-        listView.setAdapter(adapter);
+        if (str.length() != 0) {
+            Log.e("SH", "有条件查询学生表");
+            for (int i=0; i<students_list.size(); i++){
+                if (Long.toString(students_list.get(i).getSno()).equals(str)){
+                    students_list_by_sno.add(students_list.get(i));
+                }
+            }
+            StudentAdapter adapter = new StudentAdapter(StudentList.this, R.layout.student_list_item, students_list_by_sno);
+            ListView listView = findViewById(R.id.student_info_list);
+            listView.setAdapter(adapter);
+        } else {
+            Log.e("SH", "无条件查询学生表");
+            StudentAdapter adapter = new StudentAdapter(StudentList.this, R.layout.student_list_item, students_list);
+            ListView listView = findViewById(R.id.student_info_list);
+            listView.setAdapter(adapter);
+        }
     }
 }
